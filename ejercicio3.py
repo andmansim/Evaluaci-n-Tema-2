e8d3aab5636b8b478bf5ec4ec8716a2828132c75
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-07 16:41:43 +0200

    quitando staticmethod

--- ejercicio3.py
+++ ejercicio3.py
@@ -15,20 +15,20 @@ class Producto:
 class Productos:
     lista = []
     
-    @staticmethod #Se le pone a los métodos q llamamos mucho, donde solo nos guarda lo último
+    
     def buscar(nombre):
         for producto in Productos.lista: #busca en la lista que hemos creado antes
             if producto.nombre == nombre:
                 return producto
             
-    @staticmethod
+    
     def crear(codigo, nombre, precio, tipo):
         producto = Productos(codigo, nombre, precio, tipo) #llamamos a la clase Cliente
         Productos.lista.append(producto)
         Productos.guardar()
         return producto
     
-    @staticmethod
+    
     def modificar(codigo, nombre, precio, tipo):
         for indice, producto in enumerate(Productos.lista):
             if producto.nombre == nombre: #cliente nos recorre la lita, pero esta se compone de objetos de las clases, por eso podemos poner el .dni. Pq va asociado a la clase Clientes
