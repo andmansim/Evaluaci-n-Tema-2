459d14e1d96d81675caca377b29377c3ffa2191f
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-07 17:06:38 +0200

    explicaciones

--- ejercicio4.py
+++ ejercicio4.py
@@ -10,6 +10,14 @@ def errores(a, b):
         a
     except b:
         print('Error controlado')
+    if b == TypeError:
+        print('No se puede operar elementos de distintos tipos')
+    elif b == KeyError:
+        print('Ese key no está en el diccionario')
+    elif b == IndexError:
+        print('No se encuentra ese índice')
+    elif b == ZeroDivisionError:
+        print('No se puede dividir entre cero')
 
 errores(7/2, ZeroDivisionError)
 errores(lista[10],IndexError)
