c372547ca89391790cc1aa4d872650f383985e48
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-07 16:13:33 +0200

    método str

--- ejercicio3.py
+++ ejercicio3.py
@@ -5,3 +5,7 @@ class Producto:
         self.nombre = nombre
         self.precio = precio
         self.tipo = tipo
+        print('El producto ' + str(self.nombre) + ' se ha creado con éxito')
+    
+    def __str__(self):
+        return 'El producto ' + self.nombre + ' se ha creado con éxito'
