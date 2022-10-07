ab2153ca8bae5f9d5dcfa025791e0a3c15e48f30
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-07 17:02:42 +0200

    intento 4

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,19 +1,21 @@
-numero = 7/0
+
 lista = [4, 7, 30, 23, 5]
-lista[10]
 paises = { "españa":"español", "eeuu":"inglés", "italia":"italiano" } 
-paises["alemania"]
 
 
 
-resultado = "2" + 10
+
+'''resultado = "2" + 10'''
 
 def errores(a, b):
     try:
-        7/1
+        a
     except b:
         print('Error controlado')
 
-errores(numero, ZeroDivisionError)
+errores(7/2, ZeroDivisionError)
+errores(lista[10],IndexError)
+errores(paises["alemania"], KeyError)
+
 
 
