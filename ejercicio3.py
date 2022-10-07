abd4a91c1919345f957501f223ad8b277c4c65a7
andmansim <91722324+andmansim@users.noreply.github.com>
2022-10-07 16:22:50 +0200

    ejercicio 3

--- ejercicio3.py
+++ ejercicio3.py
@@ -9,3 +9,13 @@ class Producto:
     
     def __str__(self):
         return 'El producto ' + self.nombre + ' se ha creado con éxito'
+    
+    
+    @staticmethod
+    def modificar(codigo, nombre, precio, tipo):
+        for indice, cliente in enumerate(Clientes.lista):
+            if cliente.dni == dni: #cliente nos recorre la lita, pero esta se compone de objetos de las clases, por eso podemos poner el .dni. Pq va asociado a la clase Clientes
+                Clientes.lista[indice].nombre = nombre
+                Clientes.lista[indice].apellido =apellido
+                Clientes.guardar()
+                return Clientes.lista[indice]
